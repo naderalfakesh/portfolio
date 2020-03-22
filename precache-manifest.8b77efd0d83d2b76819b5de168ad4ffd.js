9d3a5192f698061dfc89083e24c07c06a80a25ee
naderalfakesh <naderalfakesh@gmail.com>
2020-03-23 01:48:30 +0300

    Updates

---
+++ precache-manifest.8b77efd0d83d2b76819b5de168ad4ffd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adf870757d85ff6073e49b04c4c5b43",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7581bd31dca74c7a3b36",
+    "url": "/portfolio/static/js/2.592dfe93.chunk.js"
+  },
+  {
+    "revision": "6ff3be09b66a6bae4c4000c31aff339c",
+    "url": "/portfolio/static/js/2.592dfe93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5272ef5d831c28f9fd1",
+    "url": "/portfolio/static/js/main.1f5d4e5d.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "4f81c7f9c4bb06ba29745a5961e8d361",
+    "url": "/portfolio/static/media/site1.4f81c7f9.jpg"
+  },
+  {
+    "revision": "2038ecec0e641a4be6dbbf05204cb757",
+    "url": "/portfolio/static/media/site2.2038ecec.jpg"
+  },
+  {
+    "revision": "9e861d616397333ea226fa371f4f50f7",
+    "url": "/portfolio/static/media/site3.9e861d61.jpg"
+  },
+  {
+    "revision": "2e2ae058da3f47ff04e00c7ec1d48a42",
+    "url": "/portfolio/static/media/spaceBg.2e2ae058.jpg"
+  }
+]);
